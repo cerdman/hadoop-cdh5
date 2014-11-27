1d4cde162226fec1dee8ed622931808d503acebf
Vincenzo D'Amore <v.damore@gmail.com>
2014-11-27 11:46:43 +0100

    fix

--- libraries/myvagrantlib.rb
+++ libraries/myvagrantlib.rb
@@ -1,9 +1,9 @@
 require 'yaml'
 
 class MyVagrantLib
-    @required_plugins = %w( vagrant-omnibus vagrant-berkshelf vagrant-lxc )
 
     def initialize()
+    	@required_plugins = %w( vagrant-omnibus vagrant-berkshelf vagrant-lxc )
     end 
 
     def check_plugins
@@ -18,14 +18,14 @@ class MyVagrantLib
 
     def get_provider(default_config)
         provider = nil
-        if !default_config.nil and !default_config.empty
+        if !default_config.nil? and !default_config.empty?
           config = YAML.load_file default_config
           provider = config['provisioner']
-          if provider.nil or provider.empty or provider == "default"
+          if provider.nil? or provider.empty? or provider == "default"
             provider = nil
           end
         end
-        if provider.nil
+        if provider.nil?
           if ARGV[1] and \
              (ARGV[1].split('=')[0] == "--provider" or ARGV[2])
             provider = (ARGV[1].split('=')[1] || ARGV[2]).to_sym
